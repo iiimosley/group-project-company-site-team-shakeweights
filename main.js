87ebb074c8102957fe0f67b377355b5fc50f2c78
Slade Utley <SladeUtley@Slades-MacBook-Pro.local>
2017-11-17 12:14:55 -0600

    altered image dimensions

--- main.js
+++ main.js
@@ -5,49 +5,49 @@ var products = [
         name: "Classic Shakeweight",
         description: "The one and only - how you got hooked on what some call 'the best exercise equipment around, if you're really serious about getting in the best shape of your life'",
         price: "$40",
-        url: "https://images-na.ssl-images-amazon.com/images/I/31yJZRTzyKL.jpg"
+        url: "img/productImages/classicShakeweight.jpg"
     },
     {
         name: "Extra Heavy Shakeweight",
         description: "For those who are super cereal",
         price: "$45",
-        url: "http://www.tvtopten.com/images/mens-shake-weight.jpg"
+        url: "img/productImages/extraHeavyShakeweight.jpg"
     },
     {
         name: "Extra Light Shakeweight",
         description: "You just really enjoy the pumping motion",
         price: "$45", 
-        url: "https://images-na.ssl-images-amazon.com/images/I/71SkKgmdzmL._SL1500_.jpg"  
+        url: "img/productImages/extraLightShakeweight.jpg"  
     },
     {
         name: "Shake Your Weight Down: Exercises Using The Shakeweight",
         description: "The ultimate guide for using that shakeweight to shake in different ways",
         price: "$25",
-        url: "http://www.shakeweightextreme.com/site_images/exercises/standing-chest.png"
+        url: "img/productImages/exerciseBook.png"
     },
     {
         name: "Excluse Shakeweight Sweatbands",
         description: "For a limited time only, let others know how much you sweat with this one of a kind sweatband",
         price: "$10",
-        url: "http://i.dailymail.co.uk/i/pix/2013/07/17/article-2366532-1AD8C954000005DC-768_634x647.jpg"
+        url: "img/productImages/sweatband.jpg"
     },
     {
         name: "Really Kewl Shakeweight T",
         description: "Just a sweet shirt to wear",
         price: "$20",
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwWv1vOVmZrlWAmYSBU-Es2eU41hFVpZ68VqQCBINZJ43B4sRpjQ"
+        url: "img/productImages/tshirt.jpeg"
     },
     {
         name: "Really Kewl Shakeweight Muscle T",
         description: "You've already given up on life",
         price: "$20",
-        url: "http://www.prana.com/media/catalog/product/w/1/w11170143_lightgreyheather_g.jpg"
+        url: "img/productImages/muscleT.jpg"
     },
     {
         name: "Exercise Specialty Gloves",
         description: "At this point, why not",
         price: "$15",
-        url: "http://odditymall.com/includes/content/upload/sponge-gloves-6756.jpg"
+        url: "img/productImages/gloves.jpg"
     }
 ]
 
